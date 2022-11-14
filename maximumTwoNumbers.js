3e14d1b47152b5e84d256913730b3dfae580ae3e
prottoy6218 <prottoy1862@gmail.com>
2022-11-14 17:38:19 +0600

    find maximum between two numbers

---
+++ maximumTwoNumbers.js
@@ -0,0 +1,19 @@
+// Write a program to find maximum between two numbers using if else.
+
+// Example
+// Input
+// Input num1: 10
+// Input num2: 20
+// Output
+
+// Maximum = 20
+
+function inputNumber(num1, num2){
+    if(num1 > num2){
+        console.log("Maximum = " + num1);
+    }
+    else
+        console.log("Maximum = " + num2);
+}
+
+inputNumber(10, 20);
